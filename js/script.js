55397a1df346644b62e39a31fe03c44d6cc5922b
Vladislav Hushcha <vladislav.hushcha@gmail.com>
2024-05-23 00:38:22 +0300

    fixes, disabled scroll with menu on, syncedcarousel drag

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ window.onload = function () {
     const menu_btn = document.querySelector('.header__menu-burger')
     const mobile_logo = document.querySelector('.header__top-logo')
     const mobile_menu = document.querySelector('.header__menu-list-mobile')
+    const body = document.querySelector('body')
 
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('active');
         mobile_logo.classList.toggle('active');
         mobile_menu.classList.toggle('active');
+        body.classList.toggle('no-scroll');
     });
 }
--- js/slider.js
+++ js/slider.js
@@ -1,47 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var splide1 = new Splide( '#splide1', {
-        type   : 'loop',
-        arrows: false,
-        pagination: false,
-        perPage: 3,
-        focus  : 0,
-        omitEnd: true,
-        gap: '28px',
-        breakpoints: {
-          992: {
-            perPage: 2,
-          },
+  var splide1 = new Splide( '#splide1', {
+      type   : 'loop',
+      arrows: false,
+      pagination: false,
+      perPage: 3,
+      focus  : 0,
+      omitEnd: true,
+      gap: '28px',
+      drag: true,
+      breakpoints: {
+        992: {
+          perPage: 2,
         },
-      } );
-      
-      splide1.mount();
+      },
+    } ).mount();
 
-      var splide2 = new Splide( '#splide2', {
-        type   : 'loop',
-        arrows: false,
-        pagination: false,
-        perPage: 3,
-        focus  : 0,
-        omitEnd: true,
-        gap: '28px',
-        breakpoints: {
-          992: {
-            perPage: 2,
-          },
+  var splide2 = new Splide( '#splide2', {
+      type   : 'loop',
+      arrows: false,
+      pagination: false,
+      perPage: 3,
+      focus  : 0,
+      omitEnd: true,
+      gap: '28px',
+      drag: true,
+      breakpoints: {
+        992: {
+          perPage: 2,
         },
-      } );
-      
-      splide2.mount();
+      },
+    } ).mount();
 
-    btnNext.addEventListener('click', e => {
-        splide1.go('+1')
-        splide2.go('+1')
-      })
-    
-    btnPrev.addEventListener('click', e => {
-        splide1.go('-1')
-        splide2.go('-1')
-      })
-    
-    });
+  splide1.on('move', function(newIndex) {
+      splide2.go(newIndex);
+  });
+
+  splide2.on('move', function(newIndex) {
+      splide1.go(newIndex);
+  });
+
+  document.getElementById('btnPrev').addEventListener('click', function() {
+    splide1.go('<');
+    splide2.go('<');
+});
+
+  document.getElementById('btnNext').addEventListener('click', function() {
+    splide1.go('>');
+    splide2.go('>');
+});
+});
+
